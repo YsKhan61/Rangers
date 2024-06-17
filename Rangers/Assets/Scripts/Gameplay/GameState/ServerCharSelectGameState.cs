1b058fc31fd17097c8507a3f93abd08166117101
Yash Sadhukhan <yskhan61@gmail.com>
2024-06-18 00:28:57 +0530

    Renamed CharSelectGameState -> ServerCharSelectGameState

--- Rangers/Assets/Scripts/Gameplay/GameState/ServerCharSelectGameState.cs
+++ Rangers/Assets/Scripts/Gameplay/GameState/ServerCharSelectGameState.cs
@@ -17,7 +17,7 @@ namespace BTG.Gameplay.GameState
     /// Server specialization of Character Select game state.
     /// </summary>
     [RequireComponent(typeof(NetcodeHooks), typeof(NetworkCharSelection))]
-    public class CharSelectGameState : GameStateBehaviour
+    public class ServerCharSelectGameState : GameStateBehaviour
     {
         [SerializeField]
         NetcodeHooks m_NetcodeHooks;
