c9abf8cc37ebfbce4a4238f13574831e999ab830
Yash Sadhukhan <yskhan61@gmail.com>
2024-06-26 17:31:48 +0530

    Create NetworkTeslaBallView

---
+++ Rangers/Assets/Scripts/Actions/PrimaryAction/TeslaFiring/NetworkTeslaBallView.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace BTG.Actions.PrimaryAction
+{
+    [RequireComponent(typeof(NetworkObject))]
+    public class NetworkTeslaBallView : TeslaBallView 
+    {
+        [SerializeField]
+        private NetworkObject m_NetworkObject;
+
+        private void Awake()
+        {
+            m_NetworkObject = GetComponent<NetworkObject>();
+        }
+    }
+
+}
+
