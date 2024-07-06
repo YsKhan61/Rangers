7ba7b508155f99f1958d7d19016cc66de881f03c
Yash Sadhukhan <yskhan61@gmail.com>
2024-07-07 00:56:48 +0530

    Update PrimaryActionFactorySO.cs

--- Rangers/Assets/Scripts/Actions/PrimaryAction/PrimaryActionFactorySO.cs
+++ Rangers/Assets/Scripts/Actions/PrimaryAction/PrimaryActionFactorySO.cs
@@ -3,8 +3,15 @@
 
 namespace BTG.Actions.PrimaryAction
 {
+    /// <summary>
+    /// This abstract class is used to create asset of the primary action factory,
+    /// as the factory is a generic class and can't be created as an asset.
+    /// </summary>
     public abstract class PrimaryActionFactorySO : FactorySO<IPrimaryAction>
     {
+        /// <summary>
+        /// Multiplayer - This method is used to get the network item from the factory
+        /// </summary>
         public abstract IPrimaryAction GetNetworkItem();
     }
 }
